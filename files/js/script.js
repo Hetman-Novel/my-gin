5821bc91dae2f83df54b8c7e64c90aade120054e
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-09-07 13:54:30 +0300

    Add files via upload

--- files/js/script.js
+++ files/js/script.js
@@ -1,29 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-   const showBlockForm = document.getElementById('show-block-form')
+   const showBlockForm = document.getElementById('show-block-form');
    if (showBlockForm) {
       showBlockForm.addEventListener('click', () => {
-         document.querySelector('.categories__blockForm').classList.add('show')
+         document.querySelector('.categories__blockForm').classList.add('show');
       })
       document.querySelector('.categories__blockForm button[type=submit]').addEventListener('click', () => {
-         showBlockForm.classList.remove('show')
+         showBlockForm.classList.remove('show');
       })
-      document.querySelector('.categories__blockForm input[type=submit]').addEventListener('click', () => {
-         showBlockForm.classList.remove('show')
-      })
+      const categoriesBlockFormSubmit = document.querySelector('.categories__blockForm input[type=submit]');
+      if (categoriesBlockFormSubmit) {
+         categoriesBlockFormSubmit.addEventListener('click', () => {
+            showBlockForm.classList.remove('show');
+         });
+      }
    }
 
-   const boxes = document.querySelectorAll('.js-same-height')
+   const boxes = document.querySelectorAll('.js-same-height');
    if (boxes) {
       let height = 0;
-      //Определяем максимальную высоту блока
       for (var i = 0; i < boxes.length; i++) {
          var current_height = boxes[i].offsetHeight;
          if (current_height > height) {
             height = current_height;
          }
       }
-      //Задаем максимальную высоту блока всем элементам
       for (var i = 0; i < boxes.length; i++) {
          boxes[i].style.height = height + 'px';
       }
